0ea2604131422a9f35ac0222f84668655cfce29d
Karolina <karolina.k.karas@gmail.com>
2022-08-27 09:09:29 +0200

    Inicjalizacja SpringBoot Application

---
+++ src/main/java/pl/cyber/trainess/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package pl.cyber.trainess.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //adnotacja, ktorej zadaniem jest uruvhomienie aplikaji Springowej
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
